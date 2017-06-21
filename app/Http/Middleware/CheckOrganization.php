d146a27244edc4f349f6144424ba9ec187a7c450
Will Jaynes <will@jaynes.org>
2017-06-21 07:26:53 -0400

    Worked on middleware stuff

---
+++ app/Http/Middleware/CheckOrganization.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Organization;
+use Closure;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+
+class CheckOrganization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $organization = $request->route('organization');
+        $success = true;
+        if (isset($organization)) {
+            $success = Auth::user()->setCurrentOrganization($request->organization);
+        }
+        if ($success)
+            return $next($request);
+        else return redirect('home');
+    }
+}
