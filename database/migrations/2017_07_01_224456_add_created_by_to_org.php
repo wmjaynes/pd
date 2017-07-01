7680aac0c64bffa53ba4d71c020c39a110e2710e
Will Jaynes <will@jaynes.org>
2017-07-01 18:59:36 -0400

    Worked on stuff

---
+++ database/migrations/2017_07_01_224456_add_created_by_to_org.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCreatedByToOrg extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->unsignedInteger('created_by')->nullable()->references('id')->on('users');
+        });
+        Schema::table('events', function (Blueprint $table) {
+            $table->unsignedInteger('created_by')->nullable()->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            //
+        });
+        Schema::table('events', function (Blueprint $table) {
+            //
+        });
+    }
+}
